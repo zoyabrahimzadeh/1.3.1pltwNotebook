b0dda9b863e366b360279464940bf6dd1e318d48
zoya <zdzhnou@gmail.com>
2021-12-08 06:12:57 -0800

    commit on ('main')

---
+++ catchingGame1.3.0.py
@@ -0,0 +1,166 @@
+# imports
+import random
+import turtle
+
+# variable initialization 
+catcherTurtle = turtle.Turtle()
+score = 0
+winning = True
+fontSetup = ("Arial", 20, "normal")
+timer = 30
+counterInterval = 1000   #1000 represents 1 second
+timerUp = False
+scoreWriter = turtle.Turtle()
+scoreWriter.penup()
+scoreWriter.hideturtle()
+scoreWriter.goto(-165, 200)
+wn = turtle.Screen()
+
+# function for countdown/timer
+def countdown(counter=turtle.Turtle()):
+  global timer, timerUp
+  # sets up the visual aspects of the timer (clearing the writing, correct spot)
+  counter.clear()
+  counter.penup()
+  counter.setpos(165, 200)
+  counter.pendown()
+  counter.hideturtle()
+  # stops the timer if the player looses or the time is up
+  if timer <= 0 or not winning:
+    counter.write("Time's Up", font=fontSetup)
+    timerUp = True
+
+    # incrementally decreases the timer so that it eventually reaches 0 
+  else:
+    counter.write("Timer: " + str(timer), font=fontSetup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counterInterval)
+
+# function for the all of movement/ functionality of the phone
+def phoneFalling(y, skyRange, phoneTurtle=turtle.Turtle(), catcher=catcherTurtle):
+    phoneTurtle.penup()
+    phoneTurtle.color("red")
+    catcher.color("purple")
+    # sets the phone appearance of turtle
+    phone = 'triangle'
+    phone2 = "ezgif-3-2a18e3e7accc.gif"
+    wn.addshape(phone2)
+    phoneTurtle.shape('triangle')
+
+    # since we can't use actual videos, we're using stop motion of different angles 
+    # to make it stimulate / appear as if theres movement
+    angle = [phone, phone, phone, phone, phone, phone]
+
+    # this would also be the x of where teresa would run to (where she drops the phone from)
+    x = random.randint(skyRange[0], skyRange[1])
+    print(x)
+    phoneTurtle.goto(x,y) # top of bridge, where the phone is dropped cords
+    phoneTurtle.setheading(260) # sets the angle so it falls downward
+    
+    # defines the variable used to repeat the while loop until the phone reaches catcher/ floor
+
+    inAir = True
+
+    # checks if the  position for the catcher is close to the position of the phone
+    # code looping phone movement will run until they are in close range of each other (collision)
+    # there are two cases in which the loop stops iterating, reaching the catcher, or the floor
+
+    while inAir: 
+        phoneTurtle.setheading(260)
+        catcherTurtle.speed(0)
+        # makes movement more randomized
+        phoneTurtle.speed(0)
+        phoneTurtle.forward(random.randint(10,30)) # moves forward a random amount before turning a random amount
+        angleTurn = random.randint(1,180) # this is so that the movement isn't linear and instead turns and staggers  
+        phoneTurtle.right(angleTurn)
+        
+    
+        for i in range(6):
+            # almost like stop motion, switches through 4 angles of person 
+            # falling to make it seem like movement
+            phoneTurtle.shape(angle[i])       
+        
+        # defines the variables that contains the current cords of catcher and phone
+        # these are used to check if there's collisions
+        xCheck, yCheck = phoneTurtle.pos() 
+        xCheckCatcher, yCheckCatcher = catcher.pos()
+ 
+        # compares the coordinates of the two turtles to see if they're near each other/ collided
+        if yCheckCatcher-30 < yCheck <yCheckCatcher + 30 and xCheckCatcher-20 < xCheck <xCheckCatcher + 20:# checks if the person is near the floor, stopping them from falling
+            inAir = False
+
+            # updates/ displays score accordingly
+            global score 
+            score +=1
+            scoreWriter.clear()
+            scoreWriter.write(score, fontSetup)
+
+        # checks if the player lost (by not catching the phone) and updatng the program accordingly   
+        if yCheckCatcher > yCheck:
+            inAir = False
+            global winning
+            winning = False
+            print("u suck") 
+
+def moveLeft(catcherTurtle=catcherTurtle):
+    catcherTurtle.penup()
+    catcherTurtle.setheading(180)
+    catcherTurtle.forward(5)
+
+
+def moveRight(catcherTurtle=catcherTurtle):
+    catcherTurtle.penup()
+    catcherTurtle.setheading(0)
+    catcherTurtle.forward(5)
+
+def runningAnimation(runFrames, rangeSpotsY, rangeSpotsX, myTurtle=turtle.Turtle()):
+    myTurtle.color("orange")
+    ySpot = random.randint(rangeSpotsY[0], rangeSpotsY[1])
+    xSpot = random.randint(rangeSpotsX[0], rangeSpotsX[1])
+    xCur, yCur = myTurtle.pos()
+    # gets the difference between these two spots to know how far we need to move
+    xDiff = int(abs(xSpot) - abs(xCur))
+    # uses iteration to move forward while doing stop motion, the extra amount of steps needed to be taken that cannot be
+    # distributed equally will be added at the end 
+    xRemainder = xDiff % 10
+    myTurtle.setpos(xCur, ySpot)
+    myTurtle.showturtle()
+    for i in range(xDiff//10):
+        for j in runFrames:
+            # moves forward the same amount while changing stop motion frames
+            # this makes everything look as efficient as possible
+            myTurtle.shape(j)
+            movement = 10/len(runFrames)
+            myTurtle.forward(movement)
+    myTurtle.forward(xRemainder)
+
+# sets up the timer
+wn.ontimer(countdown, counterInterval)
+# timerUp and winning are the two conditions where the game would be over
+# the program can only run if these conditions are not met
+
+# loops through the movement and catching/scoring of the game
+while not timerUp and winning:
+    # controls movement of the catcher
+    wn.onkeypress(moveLeft, "a")
+    wn.onkeypress(moveRight, "d") # do i have to use threading?
+    wn.listen()
+    # calls the phone movement
+    phoneFalling(200, [-100,100])
+
+# displays an ending depending on how the player performed 
+if timer <= 0:
+    # good ending
+    print("good job")
+    pass
+else:
+    # if the timer isn't 0, that means it stopped incrementing before it reached 0
+    # meaning that one of the loosing conditions was met, stopping the iteration
+    # bad ending 
+    print("ur bad")
+    pass
+
+print(score)
+
+wn = turtle.Screen()
+wn.mainloop()
